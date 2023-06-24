d7d7d2fa656fafd2a330af58e3488c51d24bff80
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-06-24 13:11:23 +0100

    Basic setup of components and screens

---
+++ src/Components/StandardSudokuGrid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StandardSudokuGrid() {
+  return (
+    <div>StandardSudokuGrid</div>
+  )
+}
+
+export default StandardSudokuGrid
---
+++ src/Screens/PuzzleScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PuzzleScreen() {
+  return (
+    <div>
+      The puzzles will go here
+      
+    </div>
+  )
+}
