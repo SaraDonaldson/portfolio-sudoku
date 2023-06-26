a41e6810e60121ca428cbe0f11d20c51f975c165
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-06-26 12:48:35 +0100

    moved the edit tile button to the game board

--- src/Components/GameBoardStandardSudoku.jsx
+++ src/Components/GameBoardStandardSudoku.jsx
@@ -153,10 +153,22 @@ function GameBoardStandardSudoku() {
 
         <div></div>
 
-        <ButtonPanelStandardSudoku
+        {/* <ButtonPanelStandardSudoku
         editTile={editTile}
-        />
+        /> */}
 
+<div className="input-buttons">
+
+            <button type="button" className="button-one" onClick={(e)=>editTile(1)}>1</button>
+            <button type="button" className="button-two" onClick={(e)=>editTile(2)}>2</button>
+            <button type="button" className="button-three" onClick={(e)=>editTile(3)}>3</button>
+            <button type="button" className="button-four" onClick={(e)=>editTile(4)}>4</button>
+            <button type="button" className="button-five" onClick={(e)=>editTile(5)}>5</button>
+            <button type="button" className="button-six" onClick={(e)=>editTile(6)}>6</button>
+            <button type="button" className="button-seven" onClick={(e)=>editTile(7)}>7</button>
+            <button type="button" className="button-eight" onClick={(e)=>editTile(8)}>8</button>
+            <button type="button" className="button-nine" onClick={(e)=>editTile(9)}>9</button>
+        </div> 
 
         <div className="checking-buttons">
             <button type="button" onClick={(e)=>resetBoard()}>Reset Game</button>
