ef5b59cc0af64446516bb768154d5a741004b27a
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-06-29 15:00:49 +0100

    reset modal created and functioning

--- src/Components/ResetModal.jsx
+++ src/Components/ResetModal.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 
-function ResetModal() {
-  return (
-    <div className='inner-reset-modal'>
-        <button className="exit-modal-btn">x</button>
-        <h3> Are you sure you want to reset the game?</h3>
-        <button className='yes-reset-btn'>Yes</button>
-        <button className='no-reset-btn'>No</button>
+function ResetModal({
+    trigger,
+    setTrigger
+}) {
+
+    function handleReset(){
+
+        setTrigger(false)
+
+    }
 
 
+  return (trigger) ? (
+    <div className='Overlay' onClick={()=> setTrigger(false)}>
+    <div className='outer-reset-modal'>
+
+    <div className='inner-reset-modal' onClick={(e)=> {e.stopPropagation()}}>
+        
+        <button 
+        className="exit-modal-btn"
+        onClick={()=> setTrigger(false)}
+        >x</button>
+
+        <h3> Are you sure you want to reset?</h3>
+        <div className='modal-select-btns-cont'>
+        <button 
+        className='yes-reset-btn'
+        onClick={()=> handleReset()}
+        >Reset</button>
+        <button 
+        className='no-reset-btn'
+        onClick={()=> setTrigger(false)}
+        >exit</button>
+        </div>
+    </div>
 
     </div>
-  )
+    </div>
+  ) : "";
 }
 
 export default ResetModal
